37a1e4329eb95a8601b90d86454905b33ac01962
karalas <kkaralas@gmail.com>
2017-03-30 01:48:38 +0300

    layer updater - ingest has times error and update does not get returned values

--- ingest/src/main/scala/demo/SentinelIngestMain.scala
+++ ingest/src/main/scala/demo/SentinelIngestMain.scala
@@ -1,72 +1,30 @@
 package demo
 
-import java.io.File
-
-import geotrellis.raster._
-import geotrellis.raster.reproject._
 import geotrellis.raster.resample.{Bilinear, NearestNeighbor}
 import geotrellis.vector.io._
 import spray.json.DefaultJsonProtocol._
-import org.apache.spark._
-import geotrellis.spark.io._
-import geotrellis.spark.io.hadoop._
 import geotrellis.spark.io.cassandra._
 import geotrellis.spark.io.cassandra.CassandraInstance
-import geotrellis.spark.io.index._
-import geotrellis.spark.etl._
-import geotrellis.spark.etl.config.EtlConf
-import org.apache.spark.SparkContext
-import geotrellis.spark.tiling._
-import geotrellis.spark.util._
 import geotrellis.spark.{ContextRDD, KeyBounds, LayerId, Metadata, SpaceTimeKey, TemporalProjectedExtent, TileLayerMetadata, TileLayerRDD}
-import geotrellis.proj4.WebMercator
-import geotrellis.spark.io.file.{FileAttributeStore, FileLayerManager, FileLayerWriter}
 import geotrellis.spark.pyramid.Pyramid
 import org.apache.spark.rdd.RDD
-import org.joda.time.DateTime
-
 import geotrellis.proj4.WebMercator
-import geotrellis.raster._
-import geotrellis.raster.resample._
-import geotrellis.spark._
-import geotrellis.spark.io._
-import geotrellis.spark.io.index._
-import geotrellis.spark.io.hadoop._
-import geotrellis.spark.io.accumulo._
-import geotrellis.raster.reproject._
-import geotrellis.spark.tiling._
 import org.apache.spark.SparkContext
 import org.joda.time.DateTime
-
 import geotrellis.raster._
-import geotrellis.raster.io.geotiff._
-import geotrellis.raster.render._
-import geotrellis.raster.resample._
-import geotrellis.raster.reproject._
-import geotrellis.proj4._
-
 import geotrellis.spark._
 import geotrellis.spark.io._
-import geotrellis.spark.io.file._
 import geotrellis.spark.io.hadoop._
 import geotrellis.spark.io.index._
-import geotrellis.spark.pyramid._
-import geotrellis.spark.reproject._
 import geotrellis.spark.tiling._
-import geotrellis.spark.render._
-
-import geotrellis.vector._
-
 import org.apache.spark._
-import org.apache.spark.rdd._
-
 import scala.io.StdIn
 import java.io.File
 
 
 object SentinelIngestMain {
 
-    // Constants
+  // Constants
   val inputPath = "file://" + new File("/home/kkaralas/Documents/vboxshare/t34tel").getAbsolutePath
   val outputPath = "data/catalog"
   val layerName = "t34tel"
